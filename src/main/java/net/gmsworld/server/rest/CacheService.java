50502e1ef07ddd56fd6f7fa57778533364c1aa10
jstakun <jstakun@redhat.com>
2015-10-23 09:22:16 +0200

    added post rest endpoint

--- src/main/java/net/gmsworld/server/rest/CacheService.java
+++ src/main/java/net/gmsworld/server/rest/CacheService.java
@@ -5,13 +5,20 @@ import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.json.JSONObject;
 
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.WriteResult;
 
 import net.gmsworld.server.mongo.DBConnection;
 
@@ -44,6 +51,16 @@ public class CacheService {
 		}
 
 		return allItemsList;
+	}
+	
+	@POST()
+	@Consumes("application/json")
+	public Response insertToCache(JSONObject document) {
+		DBCollection testing = this.getTestingCollection();
+		DBObject dbo = (DBObject) document;
+		WriteResult wr = testing.insert(dbo);
+		
+		return Response.status(200).entity("Document status: " + wr.toString()).build();
 	}
 
 }
