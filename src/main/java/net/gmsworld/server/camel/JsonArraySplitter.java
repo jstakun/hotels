3bd9d17cfa222876988b24aeb3f7362d7ec6aebb
jstakun <jstakun@redhat.com>
2015-11-11 09:56:46 +0100

    modified splitter

--- src/main/java/net/gmsworld/server/camel/JsonArraySplitter.java
+++ src/main/java/net/gmsworld/server/camel/JsonArraySplitter.java
@@ -2,16 +2,20 @@ package net.gmsworld.server.camel;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Handler;
 import org.apache.camel.Message;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class JsonArraySplitter {
 
+	private final Logger logger = Logger.getLogger(this.getClass().getName());
+	
 	@Handler
     public List<String> processMessage(Exchange exchange) {
 
@@ -20,12 +24,15 @@ public class JsonArraySplitter {
         Message message = exchange.getIn();
         String msg = message.getBody(String.class);
         
-        JSONObject root = (JSONObject) JSONValue.parse(msg);
-
+        JSONObject root = new JSONObject(msg);         
         //read first value as array
-        JSONArray jsonArray = (JSONArray) root.values().iterator().next();
-
-        for(int i=0; i<jsonArray.size(); i++) {
+        String key = (String)root.keys().next();
+        
+        logger.log(Level.INFO, "Splitting json array {0}", key);
+        
+        JSONArray jsonArray = root.getJSONArray(key);
+        
+        for(int i=0; i<jsonArray.length(); i++) {
             String jsonMsg = jsonArray.get(i).toString();
             messageList.add(jsonMsg);
         }
